803f9c8bc1310517cac8ff01cbfbb9d18ee44c31
unknown <glaisne@SENG-D-111-1201.ad.bu.edu>
2014-04-15 11:38:11 -0400

    A function repository is born.

---
+++ Write-AsCSV.ps1
@@ -0,0 +1,14 @@
+﻿function Write-AsCSV
+{
+    param([string[]] $List)
+
+    [string] $Result = ([string]::Empty)
+
+    foreach ($Entry in $List)
+    {
+        $Result += "$Entry, "
+    }
+    $Result = $Result.TrimEnd(", ")
+
+    Return $Result
+}
