b8f8001e9efd64b379a37634ebf2745ccf75e11d
Gene Laisne <glaisne@gmail.com>
2014-10-24 20:53:49 -0400

    added get-InstalledDotNetFrameworkVersions.ps1

---
+++ get-InstalledDotNetFrameworkVersions.ps1
@@ -0,0 +1,11 @@
+﻿function get-InstalledDotNetFrameworkVersions
+{
+<#
+.DESCRIPTION
+	Retruns the version numbers of all currently installed .NET framework versions
+#>
+<# This function comes from PowerShell.com's "Checking for .NET Framework Version with Powershell"
+    http://powershell.com/cs/blogs/tips/archive/2010/07/16/checking-for-net-framework-version-with-powershell.aspx
+#>
+    dir C:\Windows\Microsoft.NET\Framework\v* -name
+}
