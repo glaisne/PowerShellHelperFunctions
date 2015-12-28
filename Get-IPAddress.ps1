b0acde513e9dcf42294d2d69fd09e47d97b2ec5e
glaisne <glaisne@gmail.com>
2015-12-28 15:39:20 -0500

    Moved Get-IPAddress to PowerShellHelperFunctions repository

---
+++ Get-IPAddress.ps1
@@ -0,0 +1,46 @@
+﻿function Get-IPAddress
+{
+
+<#
+.SYNOPSIS
+	Get IP addresses for specified hostnames
+.DESCRIPTION
+	Get IP addresses for specified hostnames
+.EXAMPLE
+	"server1","server2" | Get-IPAddress |ft
+
+	This example will get the IP addresses for both hostnames.
+.EXAMPLE
+	Get-IPAddress "server1","server2" |ft
+
+	This example will get the IP addresses for both hostnames.
+.EXAMPLE
+	Get-IPAddress "www.bing.com" |ft
+
+	This example will get the IP address(es) for an individual hostname
+.EXAMPLE
+	"www.bing.com" | Get-IPAddress |ft
+
+	This example will get the IP address(es) for an individual hostname
+.PARAMETER username
+	the username to modify
+#>
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+		[string[]] $Hostname
+	)
+	begin{
+  	}
+	Process{
+        foreach ($h in $Hostname)
+        {
+            new-object PSObject -Property @{
+			    Hostname  = $h
+			    IPAddress = $([System.Net.Dns]::GetHostByName($h).addresslist)
+            }
+        }
+	}
+	end {
+	}
+}
