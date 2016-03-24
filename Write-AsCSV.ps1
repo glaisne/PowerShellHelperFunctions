777fbb4d48e09ba3cb9ee3d188800f8944a433ef
glaisne <glaisne@gmail.com>
2016-03-24 09:36:43 -0400

    Added delimiter option to write-ascsv

--- Write-AsCSV.ps1
+++ Write-AsCSV.ps1
@@ -4,16 +4,17 @@
         [Parameter(Mandatory=$true,
                    valueFromPipeline=$True,
                    Position=0)]
-            [string[]] $List
+            [string[]] $List,
+            [char]     $Delimiter = ','
     )
 
     [string] $Result = ([string]::Empty)
 
     foreach ($Entry in $List)
     {
-        $Result += "$Entry, "
+        $Result += "$Entry$Delimiter "
     }
-    $Result = $Result.TrimEnd(", ")
+    $Result = $Result.TrimEnd("$Delimiter ")
 
     Write-Output $Result
 }
